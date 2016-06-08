c1793b24700e92bb6ddac077a1b244b808dd2c23
granthum123 <granthum@live.unc.edu>
2016-06-08 00:51:35 -0400

    Added all Routes -- need to restructure into separate controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,22 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
 //Setting Domain to get around CORS
+myApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl:'home.html'
+		})
+		.when('/Civilizations', {
+			templateUrl:'civilizations.html'
+		})
+		.when('/Strategy',{
+			templateUrl:'strategy.html'
+		})
+		.when('/Tiles',{
+			templateUrl:'tiles.html'
+		})
+		.otherwise({ redirectTo:'/'})
+}]);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
